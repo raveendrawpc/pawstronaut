aeb49cffe871c5dad3f31dd96c9bae00a52721c8
Rabindra Pantha <raveendra@wpcreative.com.au>
2025-02-21 13:04:47 +0545

    Reformat

--- src/data/config.ts
+++ src/data/config.ts
@@ -7,52 +7,52 @@ export const siteSlogan = "The best pet shelter in the galaxy!";
 export const themeColor = "#FF0000";
 
 export const phone = {
-	href: "tel:+123456789",
-	label: "(123) 456-789",
+    href: "tel:+123456789",
+    label: "(123) 456-789",
 };
 
 export const email = {
-	href: "mailto:info@pawstronaut.com",
-	label: "info@pawstronaut.com",
+    href: "mailto:info@pawstronaut.com",
+    label: "info@pawstronaut.com",
 };
 
 export const address = {
-	street: "1234 Space Street",
-	city: "Galaxy City",
-	zip: "12345",
-	state: "Milky Way",
-	country: "Space",
+    street: "1234 Space Street",
+    city: "Galaxy City",
+    zip: "12345",
+    state: "Milky Way",
+    country: "Space",
 };
 
 export const socialMedia = {
-	facebook: {
-		url: "https://facebook.com",
-		label: "Facebook",
-		icon: "facebook",
-	},
-	x: {
-		url: "https://x.com",
-		label: "X",
-		icon: "x",
-	},
-	instagram: {
-		url: "https://instagram.com",
-		label: "Instagram",
-		icon: "instagram",
-	},
-	youtube: {
-		url: "https://youtube.com",
-		label: "YouTube",
-		icon: "youtube",
-	},
+    facebook: {
+        url: "https://facebook.com",
+        label: "Facebook",
+        icon: "facebook",
+    },
+    x: {
+        url: "https://x.com",
+        label: "X",
+        icon: "x",
+    },
+    instagram: {
+        url: "https://instagram.com",
+        label: "Instagram",
+        icon: "instagram",
+    },
+    youtube: {
+        url: "https://youtube.com",
+        label: "YouTube",
+        icon: "youtube",
+    },
 };
 
 export const hours = {
-	monday: "9:00 - 17:00",
-	tuesday: "9:00 - 17:00",
-	wednesday: "9:00 - 17:00",
-	thursday: "9:00 - 17:00",
-	friday: "9:00 - 17:00",
-	saturday: "9:00 - 17:00",
-	sunday: "Closed",
+    monday: "9:00 - 17:00",
+    tuesday: "9:00 - 17:00",
+    wednesday: "9:00 - 17:00",
+    thursday: "9:00 - 17:00",
+    friday: "9:00 - 17:00",
+    saturday: "9:00 - 17:00",
+    sunday: "Closed",
 };
